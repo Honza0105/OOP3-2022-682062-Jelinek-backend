6f95239b1937601225604df1727c935b53c25019
Honza0105 <honzajelinek@email.cz>
2023-04-09 13:43:49 +0200

    testing

---
+++ src/main/java/testing/learningWithCaesar.java
@@ -0,0 +1,23 @@
+package testing;
+
+import cipher.CaesarCipher;
+
+import java.util.Arrays;
+
+public class learningWithCaesar {
+    public static void main(String[] args) {
+        CaesarCipher cipher = new CaesarCipher(0);
+        String cipherEncrypted = cipher.encrypt("ABcZz, well");
+        System.out.println(cipherEncrypted);
+        System.out.println(Arrays.toString(cipherEncrypted.getBytes()));
+        System.out.println(cipher.decrypt(cipherEncrypted));
+        System.out.println(cipherEncrypted);
+        for (int i = 0; i < 30; i++) {
+            System.out.println(cipher.decrypt(cipherEncrypted));
+            cipher.increaseShift();
+        }
+        cipher.setShift(2);
+        System.out.println(cipher.decrypt(cipherEncrypted));
+
+    }
+}
